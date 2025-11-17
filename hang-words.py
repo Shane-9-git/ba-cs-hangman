f400eb62f75d465861804b8eb3a90f862ce9862a
jjwolfgang-git <github@johnwolfgang.com>
2025-11-16 20:14:38 -0500

    Added wordlist

--- hang-words.py
+++ hang-words.py
@@ -1,1 +1,3 @@
 # We keep the list of possible words for the player to guess in this file
+
+wordList = ('DOG','CAT', 'FISH', 'MOUSE', 'CAMEL', 'FERRET', 'PARROT', 'ANT', 'SPIDER', 'SQUIRREL', 'GERBAL')
