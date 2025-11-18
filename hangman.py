43d310a7232007ed81b9f0ad466d7ea076bb9c0a
princess193 <nezukos0sister@gmail.com>
2025-11-18 11:54:36 -0500

    it works

--- hangman.py
+++ hangman.py
@@ -1,2 +1,17 @@
 # hangman.py
 # This is our main program file for the Hangman activity
+
+from hang_words import wordList
+
+#initial setup
+num_guesses = 8
+word = " "
+incorrect = []
+correct = []
+
+# choose our word
+
+wordSet = set(wordList)
+word = wordSet.pop()
+
+print(word)
